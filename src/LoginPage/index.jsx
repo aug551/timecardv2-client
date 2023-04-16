df0b4ce8efd28bfdd911a3bf662d4200970f3b04
Aaron Cruz <aaroncruz_98@hotmail.com>
2023-04-15 20:50:49 -0400

    push remote

--- src/LoginPage/index.jsx
+++ src/LoginPage/index.jsx
@@ -1,30 +1,46 @@
 import './index.css'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { EmployeeContext } from '../Layout'
-import { Button, TextField } from '@mui/material';
+import { Alert, Button, TextField } from '@mui/material';
 import axios from 'axios';
 import { useNavigate, useOutletContext } from 'react-router-dom';
 
 const Login = () => {
     // const { setEmp } = useContext(EmployeeContext);
     const [emp, setEmp] = useOutletContext();
+    const [pwReq, setPwReq] = useState(false);
+    const [alerts, setAlerts] = useState([]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         let empId = e.target.empId.value;
 
+        if (pwReq && e.target.adminPw.value == "") {
+            setAlerts((prev) => [
+                ...prev,
+                <Alert severity='error'>Please enter a password to login.</Alert>
+            ]);
+        }
+
         try {
             const response = await axios.post(
                 "http://localhost:4000/emp/login/",
                 {
-                    empId
+                    empId: empId.toString()
                 }
             );
 
             if (response.status == 200) {
-                setEmp(response.data);
-                localStorage.setItem('emp', JSON.stringify(response.data));
-                // navigate('/dashboard');
+                let emp = response.data;
+
+                if (emp.ismanager) {
+                    // Show password field
+                    setPwReq(true);
+                }
+                else {
+                    setEmp(emp);
+                    localStorage.setItem('emp', JSON.stringify(emp));
+                }
             }
         }
         catch (error) {
@@ -34,9 +50,22 @@ const Login = () => {
 
     return (
         <>
+            {alerts}
             <form onSubmit={handleSubmit} id="loginForm">
                 <h1>Restaurant KIKU</h1>
                 <TextField sx={{ width: '100%' }} id="empId" label="Employee ID (ex. 12)" variant='outlined' autoComplete='off' />
+                <TextField sx={{
+                    width: '100%',
+                    display: (pwReq) ? "inline-flex" : "none",
+                }}
+                    error={pwReq}
+                    helperText="Password is required for admin"
+                    id="adminPw"
+                    type='password'
+                    label="Password"
+                    variant='outlined'
+                    autoComplete='off'
+                />
                 <Button sx={{ width: '100%', height: '2.7rem', fontSize: '1.33rem' }} variant="contained" type='submit' id="loginBtn">Login</Button>
             </form>
         </>
